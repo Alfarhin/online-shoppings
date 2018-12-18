e19c632f5bf5f2d03f6bbb732e89baf733a90de7
Alfarhin Mukadam <Alfarhin.dalvi@yahoo.com>
2018-12-18 10:08:55 +0530

    My Project Setup Completed Successfull

---
+++ shoppingsbackend/src/main/java/net/alf/shoppingsbackend/App.java
@@ -0,0 +1,13 @@
+package net.alf.shoppingsbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
